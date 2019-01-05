f0ecd556026a4991037dcdf61d5d9290f8784234
olramde <rladhkstn8@gmail.com>
2019-01-05 11:41:41 +0900

    config update

--- config.py
+++ config.py
@@ -6,7 +6,11 @@ for i in range(1, 26):
     parser.add_argument('--dataroot{}'.format(i), type=str, default='./dataset/model{}'.format(i), help='dataset path of model{}'.format(i))
     parser.add_argument('--savePathOfModel{}'.format(i), type=str, default='./models/model{}.ckpt'.format(i))
 
-defaultImageSizeList = [] # 25 tuple
+defaultImageSizeList = [(870, 100), (,), (,), (,), (,),
+                        (,), (,), (,), (,), (,),
+                        (,), (,), (,), (,), (,),
+                        (,), (,), (,), (,), (,),
+                        (,), (,), (,), (,), (,),] # 25 tuple
 for i, imgSize in enumerate(defaultImageSizeList):
     parser.add_argument('--image_size{}'.format(i+1), type=tuple, default=imgSize, help='image size of model{}'.format(i+1))
 
@@ -16,7 +20,11 @@ parser.add_argument('--num_workers', type=int, default=1, help='number of worker
 
 parser.add_argument('--num_epoch', type=int, default=5, help='number of epochs')
 
-defaultLearningRateList = [0.001, 0.001,  0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001] # 25 float
+defaultLearningRateList = [0.001, 0.001, 0.001, 0.001, 0.001,
+                           0.001, 0.001, 0.001, 0.001, 0.001,
+                           0.001, 0.001, 0.001, 0.001, 0.001,
+                           0.001, 0.001, 0.001, 0.001, 0.001,
+                           0.001, 0.001, 0.001, 0.001, 0.001] # 25 float
 for i, lr in enumerate(defaultLearningRateList):
     parser.add_argument('--lr{}'.format(i+1), type=float, default=lr, help='learning rate of model{}'.format(i+1))
 
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import train
+import config
 
+config = config.get_config()
 if __name__ == '__main__':
     train.train()
