3640bb727b1adacd58b56894fa7998e51313239a
olramde <rladhkstn8@gmail.com>
2019-01-05 15:54:00 +0900

    error update

--- dataloader.py
+++ dataloader.py
@@ -15,28 +15,225 @@ num_workers = config.num_workers
 
 class Loader:
     def __init__(self, numModel): # numModel is an integer value between 1 and 25
-        self.numModel = numModel - 1 # index operation
+        self.numModel = numModel# index operation
         self.batch_size = config.batch_size
         self.num_workers = config.num_workers
-        self.dataroot = PL.get_datarootList()[self.numModel]
-        self.imageSize = PL.get_imageSizeList()[self.numModel]
-        self.test_transform = None
-        self.train_transform = None
+        self.dataroot = PL.get_datarootList()[self.numModel-1]
+        self.imageSize = PL.get_imageSizeList()[self.numModel-1]
 
     def getLoader(self):
+        print(self.numModel)
         print("\nPreprocessing data...")
-        print(type(self.numModel))
         train_path = os.path.join(self.dataroot, 'train')
         test_path = os.path.join(self.dataroot, 'test')
 
-        self.get_TrainTransform()
-        self.get_TestTransform()
-        print(self.train_transform)
+        test_set = ImageFolder(test_path, transform=T.Compose([
+            T.Resize(self.imageSize),
+            T.ToTensor(),
+            T.Normalize(mean=(0.5, 0.5, 0.5),
+                        std=(0.5, 0.5, 0.5))
+        ]))
 
-        train_set = ImageFolder(train_path, transform=self.train_transform)
-        test_set = ImageFolder(test_path, transform=self.test_transform)
-        print(self.imageSize)
-        print(type(self.imageSize))
+        if self.numModel == 1:
+            train_set = ImageFolder(train_path, transform=T.Compose([
+                T.Resize(self.imageSize),
+                T.ToTensor(),
+                T.Normalize(mean=(0.5, 0.5, 0.5),
+                        std=(0.5, 0.5, 0.5))
+            ]))
+
+        elif self.numModel == 2:
+            train_set = ImageFolder(train_path, transform=T.Compose([
+                T.Resize(self.imageSize),
+                T.ToTensor(),
+                T.Normalize(mean=(0.5, 0.5, 0.5),
+                        std=(0.5, 0.5, 0.5))
+            ]))
+
+        elif self.numModel == 3:
+            train_set = ImageFolder(train_path, transform=T.Compose([
+                T.Resize(self.imageSize),
+                T.ToTensor(),
+                T.Normalize(mean=(0.5, 0.5, 0.5),
+                        std=(0.5, 0.5, 0.5))
+            ]))
+
+        elif self.numModel == 4:
+            train_set = ImageFolder(train_path, transform=T.Compose([
+                T.Resize(self.imageSize),
+                T.ToTensor(),
+                T.Normalize(mean=(0.5, 0.5, 0.5),
+                        std=(0.5, 0.5, 0.5))
+            ]))
+
+        elif self.numModel == 5:
+            train_set = ImageFolder(train_path, transform=T.Compose([
+                T.Resize(self.imageSize),
+                T.ToTensor(),
+                T.Normalize(mean=(0.5, 0.5, 0.5),
+                        std=(0.5, 0.5, 0.5))
+            ]))
+
+        elif self.numModel == 6:
+            train_set = ImageFolder(train_path, transform=T.Compose([
+                T.Resize(self.imageSize),
+                T.ToTensor(),
+                T.Normalize(mean=(0.5, 0.5, 0.5),
+                        std=(0.5, 0.5, 0.5))
+            ]))
+
+        elif self.numModel == 7:
+            train_set = ImageFolder(train_path, transform=T.Compose([
+                T.Resize(self.imageSize),
+                T.ToTensor(),
+                T.Normalize(mean=(0.5, 0.5, 0.5),
+                        std=(0.5, 0.5, 0.5))
+            ]))
+
+        elif self.numModel == 8:
+            train_set = ImageFolder(train_path, transform=T.Compose([
+                T.Resize(self.imageSize),
+                T.ToTensor(),
+                T.Normalize(mean=(0.5, 0.5, 0.5),
+                        std=(0.5, 0.5, 0.5))
+            ]))
+
+        elif self.numModel == 9:
+            train_set = ImageFolder(train_path, transform=T.Compose([
+                T.Resize(self.imageSize),
+                T.ToTensor(),
+                T.Normalize(mean=(0.5, 0.5, 0.5),
+                        std=(0.5, 0.5, 0.5))
+            ]))
+
+        elif self.numModel == 10:
+            train_set = ImageFolder(train_path, transform=T.Compose([
+                T.Resize(self.imageSize),
+                T.ToTensor(),
+                T.Normalize(mean=(0.5, 0.5, 0.5),
+                        std=(0.5, 0.5, 0.5))
+            ]))
+
+        elif self.numModel == 11:
+            train_set = ImageFolder(train_path, transform=T.Compose([
+                T.Resize(self.imageSize),
+                T.ToTensor(),
+                T.Normalize(mean=(0.5, 0.5, 0.5),
+                        std=(0.5, 0.5, 0.5))
+            ]))
+
+        elif self.numModel == 12:
+            train_set = ImageFolder(train_path, transform=T.Compose([
+                T.Resize(self.imageSize),
+                T.ToTensor(),
+                T.Normalize(mean=(0.5, 0.5, 0.5),
+                        std=(0.5, 0.5, 0.5))
+            ]))
+
+        elif self.numModel == 13:
+            train_set = ImageFolder(train_path, transform=T.Compose([
+                T.Resize(self.imageSize),
+                T.ToTensor(),
+                T.Normalize(mean=(0.5, 0.5, 0.5),
+                        std=(0.5, 0.5, 0.5))
+            ]))
+
+        elif self.numModel == 14:
+            train_set = ImageFolder(train_path, transform=T.Compose([
+                T.Resize(self.imageSize),
+                T.ToTensor(),
+                T.Normalize(mean=(0.5, 0.5, 0.5),
+                        std=(0.5, 0.5, 0.5))
+            ]))
+
+        elif self.numModel == 15:
+            train_set = ImageFolder(train_path, transform=T.Compose([
+                T.Resize(self.imageSize),
+                T.ToTensor(),
+                T.Normalize(mean=(0.5, 0.5, 0.5),
+                        std=(0.5, 0.5, 0.5))
+            ]))
+
+        elif self.numModel == 16:
+            train_set = ImageFolder(train_path, transform=T.Compose([
+                T.Resize(self.imageSize),
+                T.ToTensor(),
+                T.Normalize(mean=(0.5, 0.5, 0.5),
+                        std=(0.5, 0.5, 0.5))
+            ]))
+
+        elif self.numModel == 17:
+            train_set = ImageFolder(train_path, transform=T.Compose([
+                T.Resize(self.imageSize),
+                T.ToTensor(),
+                T.Normalize(mean=(0.5, 0.5, 0.5),
+                        std=(0.5, 0.5, 0.5))
+            ]))
+
+        elif self.numModel == 18:
+            train_set = ImageFolder(train_path, transform=T.Compose([
+                T.Resize(self.imageSize),
+                T.ToTensor(),
+                T.Normalize(mean=(0.5, 0.5, 0.5),
+                        std=(0.5, 0.5, 0.5))
+            ]))
+
+        elif self.numModel == 19:
+            train_set = ImageFolder(train_path, transform=T.Compose([
+                T.Resize(self.imageSize),
+                T.ToTensor(),
+                T.Normalize(mean=(0.5, 0.5, 0.5),
+                        std=(0.5, 0.5, 0.5))
+            ]))
+
+        elif self.numModel == 20:
+            train_set = ImageFolder(train_path, transform=T.Compose([
+                T.Resize(self.imageSize),
+                T.ToTensor(),
+                T.Normalize(mean=(0.5, 0.5, 0.5),
+                        std=(0.5, 0.5, 0.5))
+            ]))
+
+        elif self.numModel == 21:
+            train_set = ImageFolder(train_path, transform=T.Compose([
+                T.Resize(self.imageSize),
+                T.ToTensor(),
+                T.Normalize(mean=(0.5, 0.5, 0.5),
+                        std=(0.5, 0.5, 0.5))
+            ]))
+
+        elif self.numModel == 22:
+            train_set = ImageFolder(train_path, transform=T.Compose([
+                T.Resize(self.imageSize),
+                T.ToTensor(),
+                T.Normalize(mean=(0.5, 0.5, 0.5),
+                        std=(0.5, 0.5, 0.5))
+            ]))
+
+        elif self.numModel == 23:
+            train_set = ImageFolder(train_path, transform=T.Compose([
+                T.Resize(self.imageSize),
+                T.ToTensor(),
+                T.Normalize(mean=(0.5, 0.5, 0.5),
+                        std=(0.5, 0.5, 0.5))
+            ]))
+
+        elif self.numModel == 24:
+            train_set = ImageFolder(train_path, transform=T.Compose([
+                T.Resize(self.imageSize),
+                T.ToTensor(),
+                T.Normalize(mean=(0.5, 0.5, 0.5),
+                        std=(0.5, 0.5, 0.5))
+            ]))
+
+        elif self.numModel == 25:
+            train_set = ImageFolder(train_path, transform=T.Compose([
+                T.Resize(self.imageSize),
+                T.ToTensor(),
+                T.Normalize(mean=(0.5, 0.5, 0.5),
+                        std=(0.5, 0.5, 0.5))
+            ]))
+
         train_loader = DataLoader(train_set,
                                   batch_size=self.batch_size,
                                   shuffle=True,
@@ -48,266 +245,3 @@ class Loader:
 
         print("Completed Preprocessing!\n")
         return train_loader, test_loader
-
-    def get_TestTransform(self):
-        transform = T.Compose([
-            T.Resize(self.imageSize),
-            T.ToTensor(),
-            T.Normalize(mean=(0.5, 0.5, 0.5),
-                        std=(0.5, 0.5, 0.5))
-        ])
-        self.test_transform = transform
-        return
-
-    def get_TrainTransform(self):
-        if self.numModel == 1:
-            self.train_transform = T.Compose([
-                T.Resize(self.imageSize),
-                T.ToTensor(),
-                T.Normalize(mean=(0.5, 0.5, 0.5),
-                            std=(0.5, 0.5, 0.5))
-            ])
-            return
-
-        elif self.numModel == 2:
-            transform = T.Compose([
-                T.Resize(self.imageSize),
-                T.ToTensor(),
-                T.Normalize(mean=(0.5, 0.5, 0.5),
-                            std=(0.5, 0.5, 0.5))
-            ])
-            self.train_transform = transform
-            return
-
-        elif self.numModel == 3:
-            transform = T.Compose([
-                T.Resize(self.imageSize),
-                T.ToTensor(),
-                T.Normalize(mean=(0.5, 0.5, 0.5),
-                            std=(0.5, 0.5, 0.5))
-            ])
-            self.train_transform = transform
-            return
-
-        elif self.numModel == 4:
-            transform = T.Compose([
-                T.Resize(self.imageSize),
-                T.ToTensor(),
-                T.Normalize(mean=(0.5, 0.5, 0.5),
-                            std=(0.5, 0.5, 0.5))
-            ])
-            self.train_transform = transform
-            return
-
-        elif self.numModel == 5:
-            transform = T.Compose([
-                T.Resize(self.imageSize),
-                T.ToTensor(),
-                T.Normalize(mean=(0.5, 0.5, 0.5),
-                            std=(0.5, 0.5, 0.5))
-            ])
-            self.train_transform = transform
-            return
-
-        elif self.numModel == 6:
-            transform = T.Compose([
-                T.Resize(self.imageSize),
-                T.ToTensor(),
-                T.Normalize(mean=(0.5, 0.5, 0.5),
-                            std=(0.5, 0.5, 0.5))
-            ])
-            self.train_transform = transform
-            return
-
-        elif self.numModel == 7:
-            transform = T.Compose([
-                T.Resize(self.imageSize),
-                T.ToTensor(),
-                T.Normalize(mean=(0.5, 0.5, 0.5),
-                            std=(0.5, 0.5, 0.5))
-            ])
-            self.train_transform = transform
-            return
-
-        elif self.numModel == 8:
-            transform = T.Compose([
-                T.Resize(self.imageSize),
-                T.ToTensor(),
-                T.Normalize(mean=(0.5, 0.5, 0.5),
-                            std=(0.5, 0.5, 0.5))
-            ])
-            self.train_transform = transform
-            return
-
-        elif self.numModel == 9:
-            transform = T.Compose([
-                T.Resize(self.imageSize),
-                T.ToTensor(),
-                T.Normalize(mean=(0.5, 0.5, 0.5),
-                            std=(0.5, 0.5, 0.5))
-            ])
-            self.train_transform = transform
-            return
-
-        elif self.numModel == 10:
-            transform = T.Compose([
-                T.Resize(self.imageSize),
-                T.ToTensor(),
-                T.Normalize(mean=(0.5, 0.5, 0.5),
-                            std=(0.5, 0.5, 0.5))
-            ])
-            self.train_transform = transform
-            return
-
-        elif self.numModel == 11:
-            transform = T.Compose([
-                T.Resize(self.imageSize),
-                T.ToTensor(),
-                T.Normalize(mean=(0.5, 0.5, 0.5),
-                            std=(0.5, 0.5, 0.5))
-            ])
-            self.train_transform = transform
-            return
-            return transform
-
-        elif self.numModel == 12:
-            transform = T.Compose([
-                T.Resize(self.imageSize),
-                T.ToTensor(),
-                T.Normalize(mean=(0.5, 0.5, 0.5),
-                            std=(0.5, 0.5, 0.5))
-            ])
-            self.train_transform = transform
-            return
-
-        elif self.numModel == 13:
-            transform = T.Compose([
-                T.Resize(self.imageSize),
-                T.ToTensor(),
-                T.Normalize(mean=(0.5, 0.5, 0.5),
-                            std=(0.5, 0.5, 0.5))
-            ])
-            self.train_transform = transform
-            return
-
-        elif self.numModel == 14:
-            transform = T.Compose([
-                T.Resize(self.imageSize),
-                T.ToTensor(),
-                T.Normalize(mean=(0.5, 0.5, 0.5),
-                            std=(0.5, 0.5, 0.5))
-            ])
-            self.train_transform = transform
-            return
-
-        elif self.numModel == 15:
-            transform = T.Compose([
-                T.Resize(self.imageSize),
-                T.ToTensor(),
-                T.Normalize(mean=(0.5, 0.5, 0.5),
-                            std=(0.5, 0.5, 0.5))
-            ])
-            self.train_transform = transform
-            return
-
-        elif self.numModel == 16:
-            transform = T.Compose([
-                T.Resize(self.imageSize),
-                T.ToTensor(),
-                T.Normalize(mean=(0.5, 0.5, 0.5),
-                            std=(0.5, 0.5, 0.5))
-            ])
-            self.train_transform = transform
-            return
-
-        elif self.numModel == 17:
-            transform = T.Compose([
-                T.Resize(self.imageSize),
-                T.ToTensor(),
-                T.Normalize(mean=(0.5, 0.5, 0.5),
-                            std=(0.5, 0.5, 0.5))
-            ])
-            self.train_transform = transform
-            return
-
-        elif self.numModel == 18:
-            transform = T.Compose([
-                T.Resize(self.imageSize),
-                T.ToTensor(),
-                T.Normalize(mean=(0.5, 0.5, 0.5),
-                            std=(0.5, 0.5, 0.5))
-            ])
-            self.train_transform = transform
-            return
-
-        elif self.numModel == 19:
-            transform = T.Compose([
-                T.Resize(self.imageSize),
-                T.ToTensor(),
-                T.Normalize(mean=(0.5, 0.5, 0.5),
-                            std=(0.5, 0.5, 0.5))
-            ])
-            self.train_transform = transform
-            return
-
-        elif self.numModel == 20:
-            transform = T.Compose([
-                T.Resize(self.imageSize),
-                T.ToTensor(),
-                T.Normalize(mean=(0.5, 0.5, 0.5),
-                            std=(0.5, 0.5, 0.5))
-            ])
-            self.train_transform = transform
-            return
-
-        elif self.numModel == 21:
-            transform = T.Compose([
-                T.Resize(self.imageSize),
-                T.ToTensor(),
-                T.Normalize(mean=(0.5, 0.5, 0.5),
-                            std=(0.5, 0.5, 0.5))
-            ])
-            self.train_transform = transform
-            return
-
-        elif self.numModel == 22:
-            transform = T.Compose([
-                T.Resize(self.imageSize),
-                T.ToTensor(),
-                T.Normalize(mean=(0.5, 0.5, 0.5),
-                            std=(0.5, 0.5, 0.5))
-            ])
-            self.train_transform = transform
-            return
-
-        elif self.numModel == 23:
-            transform = T.Compose([
-                T.Resize(self.imageSize),
-                T.ToTensor(),
-                T.Normalize(mean=(0.5, 0.5, 0.5),
-                            std=(0.5, 0.5, 0.5))
-            ])
-            self.train_transform = transform
-            return
-
-        elif self.numModel == 24:
-            transform = T.Compose([
-                T.Resize(self.imageSize),
-                T.ToTensor(),
-                T.Normalize(mean=(0.5, 0.5, 0.5),
-                            std=(0.5, 0.5, 0.5))
-            ])
-            self.train_transform = transform
-            return
-
-        elif self.numModel == 25:
-            transform = T.Compose([
-                T.Resize(self.imageSize),
-                T.ToTensor(),
-                T.Normalize(mean=(0.5, 0.5, 0.5),
-                            std=(0.5, 0.5, 0.5))
-            ])
-            self.train_transform = transform
-            return
-
-
