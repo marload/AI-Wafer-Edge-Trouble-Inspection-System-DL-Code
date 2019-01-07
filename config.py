53d2bf92e6e6793894271aa9aede2b1847916493
olramde <rladhkstn8@gmail.com>
2019-01-08 02:49:01 +0900

    default image size update

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ for i in range(1, 26):
     parser.add_argument('--dataroot{}'.format(i), type=str, default='./dataset/model{}'.format(i), help='dataset path of model{}'.format(i))
     parser.add_argument('--savePathOfModel{}'.format(i), type=str, default='./models/model{}.ckpt'.format(i))
 
-defaultImageSizeList = [(867, 195), (867, 200), (867, 187), (867, 170), (867,170),
-                        (867,163), (867,156), (867,149), (867,141), (867,138),
-                        (867,136), (867,135), (867,134), (867,131), (867,129),
-                        (867,128), (867,124), (867,121), (867,122), (867,122),
-                        (867,123), (867,120), (867,118), (867,115), (867,102)] # 25 tuple
+defaultImageSizeList = [(500, 195), (500, 200), (500, 187), (500, 170), (500,170),
+                        (500, 163), (500, 156), (500, 149), (500, 141), (500, 138),
+                        (500, 136), (500, 135), (500, 134), (500, 131), (500, 129),
+                        (500, 128), (500, 124), (500, 121), (500, 122), (500, 122),
+                        (500, 123), (500, 120), (500, 118), (500, 115), (500, 102)]
 for i, imgSize in enumerate(defaultImageSizeList):
     parser.add_argument('--image_size{}'.format(i+1), type=tuple, default=imgSize, help='image size of model{}'.format(i+1))
 
